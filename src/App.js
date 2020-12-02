af9b7b9f2e272e6e260de8e73e6b7b5705ac0347
Greg <g.rutherford10@gmail.com>
2020-12-02 10:46:02 +0000

    add function to retrieve random quote & author

--- src/App.js
+++ src/App.js
@@ -1,13 +1,39 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      quoteText: "",
+      quoteAuthor: ""
+    }
+  }
+
+  componentDidMount() {
+    axios.get('https://quote-garden.herokuapp.com/api/v2/quotes/random')
+    .then(response => this.setState({
+      quoteText: response.data.quote.quoteText,
+      quoteAuthor: response.data.quote.quoteAuthor
+    }))
+    .catch(error => console.error(error))
+  }
+
   render() {
+    const {quoteText, quoteAuthor} = this.state;
     return(
       <div className="app-container">
         <div className="quote">
           <div id="quote-line"></div>
-          <h2>Quote will go here</h2>
+          {quoteText.length > 0 && 
+          <div>
+            <h2>{quoteText}</h2>
+          <button id="quote-author">
+            {quoteAuthor}
+          </button>
+          </div>
+          }
         </div>
       </div>
     )
