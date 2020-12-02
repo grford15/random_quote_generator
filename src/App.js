cb95604bea4ce1216362c9eb259df9ce50c670ad
Greg <g.rutherford10@gmail.com>
2020-12-02 15:21:50 +0000

    add in react-router & basic home page

--- src/App.js
+++ src/App.js
@@ -1,34 +1,54 @@
-import React, { Component } from 'react';
-import axios from 'axios';
+import React, { Component } from "react";
+import axios from "axios";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import Home from "./components/Home";
 import { FaRandom } from "react-icons/fa";
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      quoteText: "",
-      quoteAuthor: "",
-      quoteGenre: ""
-    }
-  }
+	constructor(props) {
+		super(props);
+		this.state = {
+			quoteText: "",
+			quoteAuthor: "",
+			quoteGenre: "",
+		};
+	}
 
-  componentDidMount() {
-    axios.get('https://quote-garden.herokuapp.com/api/v2/quotes/random')
-    .then(response => this.setState({
-      quoteText: response.data.quote.quoteText,
-      quoteAuthor: response.data.quote.quoteAuthor,
-      quoteGenre: response.data.quote.quoteGenre
-    }))
-    .catch(error => console.error(error))
-  }
+	componentDidMount() {
+		axios
+			.get("https://quote-garden.herokuapp.com/api/v2/quotes/random")
+			.then((response) =>
+				this.setState({
+					quoteText: response.data.quote.quoteText,
+					quoteAuthor: response.data.quote.quoteAuthor,
+					quoteGenre: response.data.quote.quoteGenre,
+				})
+			)
+			.catch((error) => console.error(error))
+	}
 
-  render() {
-    const {quoteText, quoteAuthor, quoteGenre} = this.state;
-    return(
-      <div className="app-container">
-        <div id="logo">RandomQuotes <FaRandom onClick={() => window.location.reload(false)}/></div>
-        
+	render() {
+		const { quoteText, quoteAuthor, quoteGenre } = this.state;
+		return (
+			<Router>
+				<div className="app-container">
+					<div id="logo">
+						RandomQuotes{" "}
+						<FaRandom onClick={() => window.location.reload(false)} />
+					</div>
+					<Route
+						exact
+						path="/"
+						render={() => (
+							<Home
+								quoteText={quoteText}
+								quoteAuthor={quoteAuthor}
+								quoteGenre={quoteGenre}
+							/>
+						)}
+					/>
+					{/*        
           {quoteText.length > 0 && 
           <div>
             <div className="quote">
@@ -40,11 +60,11 @@ class App extends Component {
             <h6>{quoteGenre}</h6>
           </div>
           </div>
-          }
-        </div>
-      
-    )
-  }
+          } */}
+				</div>
+			</Router>
+		);
+	}
 }
 
 export default App;
